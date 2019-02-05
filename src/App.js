d2ed5da15daf14ec8ed363111e9c53c638679054
Taras <taras.prezlyata.sign.in@gmail.com>
2019-02-05 03:09:57 +0200

    react-search

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const API_URL =
+	'https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=6e7e169c9890dd483c8b55ec4a4336e5&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1&auth_token=72157703070794842-6c1ce4456a587370&api_sig=13d0a05385a8ab8e4fc85a253166530c';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			initialPhotos: [],
+			photos: []
+		};
+	}
+
+	componentDidMount() {
+		this.getPhotos();
+	}
+
+	getPhotos = () => {
+		fetch(API_URL)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				this.setState({ initialPhotos: data.photos.photo, photos: data.photos.photo });
+			});
+	};
+
+	filterPhotos = (e) => {
+		let updatedPhotos = this.state.initialPhotos;
+
+		updatedPhotos = updatedPhotos.filter((item) => {
+			return item.title.toLowerCase().search(e.target.value.toLowerCase()) !== -1;
+		});
+
+		this.setState({ photos: updatedPhotos });
+	};
+
+	render() {
+		console.log(this.state.photos);
+		return (
+			<div className="app-photos">
+				<div className="container">
+					<div className="findCard">
+						<input placeholder="Search..." onChange={this.filterPhotos} />
+					</div>
+					<div className="cards">
+						{this.state.photos.map((photo, idx) => {
+							return (
+								<div className="card">
+									<div key={idx}>
+										<img
+											src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+											alt="img"
+										/>
+										<div className="discard">
+											<h5>{photo.title}</h5>
+										</div>
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
